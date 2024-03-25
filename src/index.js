b09deaa778e602976a7d8acc9ebd27fbc46e19bd
Bruno-Calasans-ifpe <brcl1@discente.ifpe.edu.br>
2024-03-25 13:38:41 -0300

    create Produto route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import { PORT } from "./config/constants.js"
 import CategoriaRouter from "./routers/Categoria.router.js"
 import ClienteRouter from "./routers/Cliente.router.js"
+import ProdutoRouter from "./routers/Produto.router.js"
 
 const app = express()
 app.use(express.json())
@@ -9,6 +10,7 @@ app.use(express.json())
 // routers
 app.use("/categoria", CategoriaRouter)
 app.use("/cliente", ClienteRouter)
+app.use("/produto", ProdutoRouter)
 
 app.get("/", (req, res) => {
   res.json("Server is online")
