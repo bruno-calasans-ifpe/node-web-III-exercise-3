b0490c3eada80f324a58a6c808c65f4dbe4cf80e
Bruno-Calasans-ifpe <brcl1@discente.ifpe.edu.br>
2024-03-25 14:14:07 -0300

    create ItemPedido route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import CategoriaRouter from "./routers/Categoria.router.js"
 import ClienteRouter from "./routers/Cliente.router.js"
 import ProdutoRouter from "./routers/Produto.router.js"
 import PedidoRouter from "./routers/Pedido.router.js"
+import ItemPedido from "./routers/ItemPedido.router.js"
 
 const app = express()
 app.use(express.json())
@@ -13,6 +14,7 @@ app.use("/categoria", CategoriaRouter)
 app.use("/cliente", ClienteRouter)
 app.use("/produto", ProdutoRouter)
 app.use("/pedido", PedidoRouter)
+app.use("/item-pedido", ItemPedido)
 
 app.get("/", (req, res) => {
   res.json("Server is online")
